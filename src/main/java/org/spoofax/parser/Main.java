9b3b01930e7fe1221f4ee78259396ccabad0c241
Karl Trygve Kalleberg <karltk@strategoxt.org>
2011-12-31 15:31:48 +0100

    Initial import of skeleton for SDF parse table generator.

---
+++ src/main/java/org/spoofax/parser/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2011, Karl Trygve Kalleberg <karltk near strategoxt dot org>
+ *
+ * Licensed under the GNU Lesser General Public License, v2.1
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Parse table generator");
+    }
+}
+
